b557de0aba5bede9d544761533bfe430b735379d
liaspas <hlias_pasx@hotmail.com>
2023-03-10 17:02:48 +0200

    onnx export fix

--- src/setfit/exporters/onnx.py
+++ src/setfit/exporters/onnx.py
@@ -60,14 +60,8 @@ class OnnxSetFitModel(torch.nn.Module):
 
         # If head is set then we have a fully torch based model and make the final predictions
         # with the head.
-        out_probs = self.model_head(embeddings)
-        # one target or multilabel
-        if self.model_head.out_features == 1 or self.model_head.multitarget:
-            out_probs = torch.sigmoid(out_probs)
-            out = torch.where(out_probs >= 0.5, 1, 0)
-        else:  # multi-class
-            out = torch.argmax(out_probs, dim=-1)
-        return out, out_probs
+        out = self.model_head(embeddings)
+        return out
 
 
 def export_onnx_setfit_model(setfit_model: OnnxSetFitModel, inputs, output_path, opset: int = 12):
